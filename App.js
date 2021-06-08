9cf8e476d0f85286d4ca1513041e11366e7e93d9
beatriztc56 <78417194+beatriztc56@users.noreply.github.com>
2021-06-08 18:48:09 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from "./goodvibing_logo_ps02.png";
+import Login from "./Login";
+import Register from "./Register";
+//import "./App.css";
+//import ErrorBoundary from "./ErrorBoundary";
+
+function App() {
+  return (
+    <div className="App">
+      <Register />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
